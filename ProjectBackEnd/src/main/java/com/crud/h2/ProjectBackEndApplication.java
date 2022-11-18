0d1cbb0b3eca03f7a3aa342c88f28805622e7f92
Dani Martos <dani.martos69@gmail.com>
2022-11-18 12:37:30 +0100

    c

--- ProjectBackEnd/src/main/java/com/crud/h2/ProjectBackEndApplication.java
+++ ProjectBackEnd/src/main/java/com/crud/h2/ProjectBackEndApplication.java
@@ -1,4 +1,4 @@
-package com.crud.h2.main;
+package com.crud.h2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- ProjectBackEnd/src/main/java/com/crud/h2/ServletInitializer.java
+++ ProjectBackEnd/src/main/java/com/crud/h2/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.crud.h2.main;
+package com.crud.h2;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
