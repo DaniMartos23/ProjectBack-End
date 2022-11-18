807e1172f4e5147a7fcf725481cabae122ea4a95
Dani Martos <dani.martos69@gmail.com>
2022-11-18 11:42:37 +0100

    cambio main

--- ProjectBackEnd/src/main/java/com/crud/h2/WebConfiguration.java
+++ ProjectBackEnd/src/main/java/com/crud/h2/WebConfiguration.java
@@ -1,4 +1,4 @@
-package com.crud.h2.main;
+package com.crud.h2;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
