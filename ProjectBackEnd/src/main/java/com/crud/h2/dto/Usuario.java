75bb7e5e53ba8a14e82da2f7f84cf0de79f4e720
Dani Martos <dani.martos69@gmail.com>
2022-12-20 13:57:43 +0100

    json usuario

--- ProjectBackEnd/src/main/java/com/crud/h2/dto/Usuario.java
+++ ProjectBackEnd/src/main/java/com/crud/h2/dto/Usuario.java
@@ -100,11 +100,11 @@ public class Usuario {
 	
 	
 	//Getters y Setters
-	
+	@JsonIgnore
 	public String getUsuario() {
 		return usuario;
 	}
-
+	@JsonIgnore
 	public void setUsuario(String usuario) {
 		this.usuario = usuario;
 	}
