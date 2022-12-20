3907644785f6d32e84ffd9ab05cd26ee7cb2022a
juanXxue6 <juanxue6@gmail.com>
2022-12-20 19:55:22 +0100

    C

--- ProjectBackEnd/src/main/java/com/crud/h2/controller/ReservaController.java
+++ ProjectBackEnd/src/main/java/com/crud/h2/controller/ReservaController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.crud.h2.dto.Reserva;
 import com.crud.h2.service.ReservaServiceImpl;
+import com.crud.h2.service.UsuarioServiceImpl;
 
 
 
@@ -23,6 +24,9 @@ public class ReservaController {
 	
 	@Autowired
 	ReservaServiceImpl reservaServiceImpl;
+	
+	@Autowired
+	UsuarioServiceImpl usuarioServiceImpl;
 	
 	@GetMapping("/reservas")
 	public List<Reserva> listarReservas(){
@@ -37,8 +41,11 @@ public class ReservaController {
 	
 	
 	
-	@PostMapping("/reservas")
-	public Reserva salvarReserva(@RequestBody Reserva reserva) {
+	@PostMapping("/reservas/post/usuario")
+	public Reserva salvarReserva(@PathVariable(name="usuario") String usuario,@RequestBody Reserva reserva) {
+		
+		
+		reserva.setId(usuarioServiceImpl.MostrarUsuarioPorUsuario(usuario).getId());
 		return reservaServiceImpl.guardarReserva(reserva);
 	}
 	
